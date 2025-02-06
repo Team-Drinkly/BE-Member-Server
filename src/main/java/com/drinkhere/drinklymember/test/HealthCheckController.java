e9c9480fd1d966f1364becd51f7b068914932a33
안정후 <austinan@naver.com>
2025-02-06 20:37:25 +0900

    fix: member url fix

--- src/main/java/com/drinkhere/drinklymember/test/HealthCheckController.java
+++ src/main/java/com/drinkhere/drinklymember/test/HealthCheckController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/member")
 public class HealthCheckController {
 
     @GetMapping("/health-check")
