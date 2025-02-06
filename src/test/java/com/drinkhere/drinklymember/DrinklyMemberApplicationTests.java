48b9943476af04f4161c3f607c14aa7bd2ac99e7
안정후 <austinan@naver.com>
2025-02-06 17:18:50 +0900

    chore: settings for user micro service

---
+++ src/test/java/com/drinkhere/drinklymember/DrinklyMemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drinkhere.drinklymember;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrinklyMemberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
