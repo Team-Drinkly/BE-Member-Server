4bb0b427174c8c19ce53e6ac61f09a7d62080c35
안정후 <austinan@naver.com>
2025-02-06 19:44:57 +0900

    test: user service health-check

---
+++ src/main/java/com/drinkhere/drinklymember/test/HealthCheckController.java
@@ -0,0 +1,15 @@
+package com.drinkhere.drinklymember.test;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HealthCheckController {
+
+    @GetMapping("/health-check")
+    public String status() {
+        return "유저 마이크서비스 테스트입니다.";
+    }
+}
