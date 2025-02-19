8fc030b7fb32be06a0282c1318dd0417b9065cb8
안정후 <austinan@naver.com>
2025-02-19 16:35:33 +0900

    refactor: 권한에 맞게 경로 변경

--- src/main/java/com/drinkhere/drinklymember/application/subscribe/presentation/MemberSubscribeController.java
+++ src/main/java/com/drinkhere/drinklymember/application/subscribe/presentation/MemberSubscribeController.java
@@ -6,7 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/v1/member/subscribe")
+@RequestMapping("/api/v1/member/m")
 @RequiredArgsConstructor
 public class MemberSubscribeController {
 
@@ -15,7 +15,7 @@ public class MemberSubscribeController {
     /**
      * 구독 상태 업데이트 API (구독 이력 ID 및 기간 반영)
      */
-    @PostMapping("/update")
+    @PostMapping("/subscribe-update")
     public ApplicationResponse<String> updateSubscriptionStatus(
             @RequestHeader("member-id") Long memberId,
             @RequestParam("subscription-history-id") Long subscriptionHistoryId,
